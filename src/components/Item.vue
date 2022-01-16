5602baf873544f5c42382afb517163806b544350
baran can <barancanatbas102@gmail.com>
2022-01-16 20:52:27 +0300

    add: todo app

---
+++ src/components/Item.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-row class="bg-success mt-2 p-4 col-md-12">
+        lorem
+    </b-row>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container class="mt-5">
+    <h1 class="title"> <span>#</span> Todo</h1>
+    <b-row class="mt-5">
+      <b-col class="col-md-11">
+        <b-input-group >
+          <b-form-input></b-form-input>
+        </b-input-group>
+      </b-col>
+      <b-col class="col-md-1">
+        <b-button variant="info">Add</b-button>
+      </b-col>
+    </b-row>
+    <b-row class="col-md-10 mt-5 mx-auto">
+      <item></item>
+      <item></item>
+    </b-row>
+      
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import item from "@/components/Item.vue"
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components:{
+    item
   }
 }
 </script>
+
+<style scoped>
+.title span{
+  visibility: hidden;
+}
+.title:hover span{
+  visibility: visible;
+  color: rgb(62, 62, 129);
+}
+</style>
